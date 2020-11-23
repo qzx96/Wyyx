8b21047efcbc9de00425c51311c82bf22101e3fe
qzx96 <1319400147@qq.com>
2020-11-23 22:01:42 +0800

    2020-11-23

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
-document.querySelector('h1').onclick = function (){
-    alert(123);
-  };
+// var $li = $('.li_hover')
+// console.log($li)
+$('.li_hover').on({
+  mouseenter:function(){
+    $('.down').animate({
+        // opacity:'1'
+    })
+    $(this).find('.down').stop().show()
+  },
+  mouseleave:function(){
+    $('.down').animate({
+      // opacity:'0'
+    })
+    $(this).find('.down').stop().hide()
+  }
+})
